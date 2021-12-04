3babc2fafac398ff6b328568f3a161ad8a4018c4
parkhan <6987410@naver.com>
2021-12-04 15:13:29 +0900

    알고리즘 너무 어렵다

---
+++ src/baekjoon/math2/Main02.java
@@ -0,0 +1,17 @@
+package baekjoon.math2;
+
+import java.util.Scanner;
+
+public class Main02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int w = scanner.nextInt();
+        int h = scanner.nextInt();
+
+        int xm = Math.min(x, w - x);
+        int ym = Math.min(y, h - y);
+        System.out.println(Math.min(xm, ym));
+    }
+}
---
+++ src/baekjoon/math2/Main06.java
@@ -0,0 +1,18 @@
+package baekjoon.math2;
+
+import java.util.Scanner;
+
+public class Main06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
+            int r1 = scanner.nextInt();
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
+            int r2 = scanner.nextInt();
+        }
+    }
+}
