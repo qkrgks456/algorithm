a035a887af7823166bbccafc7b838573fd2518e3
parkhan <6987410@naver.com>
2021-12-11 14:17:58 +0900

    알고리즘 너무 어렵다

--- src/baekjoon/recursion/Main03.java
+++ src/baekjoon/recursion/Main03.java
@@ -1,15 +1,24 @@
 package baekjoon.recursion;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main03 {
     static char[][] chars;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         chars = new char[n][n];
         recursion(0, 0, n, false);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < n; i++) {
+            bufferedWriter.write(chars[i]);
+            bufferedWriter.write("\n");
+        }
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 
     public static void recursion(int x, int y, int n, boolean check) {
---
+++ src/baekjoon/recursion/Main04.java
@@ -0,0 +1,32 @@
+package baekjoon.recursion;
+
+import java.util.Scanner;
+
+public class Main04 {
+
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        stringBuilder.append(((int) Math.pow(2, n) - 1) + "\n");
+        recursion(n, 1, 2, 3);
+        System.out.println(stringBuilder);
+    }
+
+    // n=k
+    // n=k-1
+    public static void recursion(int n, int start, int assist, int result) {
+        if (n == 1) {
+            stringBuilder.append(start + " " + result + "\n");
+            return;
+        } else {
+            // A -> B N-1개
+            recursion(n - 1, start, result, assist);
+            // A -> C 실제 이동 1개
+            stringBuilder.append(start + " " + result + "\n");
+            // B -> C N-1개
+            recursion(n - 1, assist, start, result);
+        }
+    }
+}
