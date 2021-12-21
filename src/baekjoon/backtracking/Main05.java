4372e5bf573a1b3a9049660e87573d868ae70a70
qkrgks456 <6987410@naver.com>
2021-12-21 15:56:16 +0900

    백트래킹 기본문제는 할만한것 같은데 이제 지옥의 N-Queen 문제를 해결해보자구

---
+++ src/baekjoon/backtracking/Main05.java
@@ -0,0 +1,11 @@
+package baekjoon.backtracking;
+
+import java.util.Scanner;
+
+public class Main05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] ints = new int[n][n];
+    }
+}
