e7c7b2530e37c4e88e5f50ac50a6f60fd16bb6ff
parkhan <6987410@naver.com>
2021-12-23 21:17:04 +0900

    와 파도반 수열이랑 01타일 풀이 안보다니 DP 개재밌네

--- src/baekjoon/dp1/Main03.java
+++ src/baekjoon/dp1/Main03.java
@@ -4,29 +4,26 @@ import java.util.Scanner;
 
 public class Main03 {
     public static int n, count;
-    public static int[] ints, ch;
+    public static int[] ch;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
-        ints = new int[n + 1];
-        ch = new int[n];
-        DFS(-1, 0);
-        System.out.println(count / 2);
+        ch = new int[n + 1];
+        System.out.println(DFS(n));
+
     }
 
-    public static void DFS(int depth, int num) {
-        if (depth == n) {
-            count++;
-            for (int i = 1; i < ints.length; i++) {
-                System.out.print(ints[i]);
-            }
-            System.out.println();
-            return;
+    public static int DFS(int num) {
+        if (ch[num] != 0) {
+            return ch[num];
+        }
+        if (num == 1) {
+            return 1;
+        } else if (num == 2) {
+            return 2;
         } else {
-            ints[depth + 1] = num;
-            DFS(depth + 1, 1);
-            DFS(depth + 1, 0);
+            return ch[num] = (DFS(num - 1) + DFS(num - 2)) % 15746;
         }
     }
 }
