2eecb866d75dd8f098f11c1231dc924bf6ca7972
qkrgks456 <6987410@naver.com>
2021-12-23 18:11:37 +0900

    00타일 기다려라 박살내줄게

--- src/baekjoon/dp1/Main03.java
+++ src/baekjoon/dp1/Main03.java
@@ -1,7 +1,32 @@
 package baekjoon.dp1;
 
-public class Main03 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main03 {
+    public static int n, count;
+    public static int[] ints, ch;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        ints = new int[n + 1];
+        ch = new int[n];
+        DFS(-1, 0);
+        System.out.println(count / 2);
+    }
+
+    public static void DFS(int depth, int num) {
+        if (depth == n) {
+            count++;
+            for (int i = 1; i < ints.length; i++) {
+                System.out.print(ints[i]);
+            }
+            System.out.println();
+            return;
+        } else {
+            ints[depth + 1] = num;
+            DFS(depth + 1, 1);
+            DFS(depth + 1, 0);
+        }
     }
 }
