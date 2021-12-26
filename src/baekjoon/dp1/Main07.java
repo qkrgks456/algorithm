40d687b395798d3b1cecb1e2befc6efb30340f34
parkhan <6987410@naver.com>
2021-12-26 22:59:35 +0900

    dp 역시 어렵네

--- src/baekjoon/dp1/Main07.java
+++ src/baekjoon/dp1/Main07.java
@@ -3,7 +3,6 @@ package baekjoon.dp1;
 import java.util.Scanner;
 
 public class Main07 {
-    static int root, n;
     static int[] ints;
     static int[] ch;
 
@@ -11,21 +10,30 @@ public class Main07 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         ints = new int[n + 1];
+        ch = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             ints[i] = scanner.nextInt();
         }
-        int count = 0;
-        int result = 0;
-        int num = 0;
-        while (true) {
-            if (ints[num + 1] > ints[num + 2]) {
-                count++;
-                num++;
-            } else {
-                count = 0;
-                num += 2;
-            }
-            result += Math.max(ints[num + 1], ints[num + 2]);
+        ch[0] = ints[0];
+        ch[1] = ints[1];
+        if (n >= 2) {
+            ch[2] = ints[1] + ints[2];
         }
+        // f(x) = max(g(x),h(x))
+        // g(x) = h(x-1) + arr[x]
+        // h(x) = max(g(x-2),h(x-2)) + arr[x]
+        // = f(x-2) + arr[x]
+        System.out.println(DFS(n));
+    }
+
+
+    public static int DFS(int num) {
+        if (ch[num] != 0) {
+            return ch[num];
+        }
+        if (num == 0) {
+            return 0;
+        }
+        return ch[num] = Math.max(DFS(num - 2), DFS(num - 3) + ints[num - 1]) + ints[num];
     }
 }
---
+++ src/baekjoon/dp1/Main08.java
@@ -0,0 +1,22 @@
+package baekjoon.dp1;
+
+import java.util.Scanner;
+
+public class Main08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] ints = new int[n + 1];
+        for (int i = 2; i < ints.length; i++) {
+            ints[i] = ints[i - 1] + 1;
+            if (i % 3 == 0) {
+                ints[i] = Math.min(ints[i / 3] + 1, ints[i]);
+            }
+            if (i % 2 == 0) {
+                ints[i] = Math.min(ints[i / 2] + 1, ints[i]);
+            }
+
+        }
+        System.out.println(ints[n]);
+    }
+}
