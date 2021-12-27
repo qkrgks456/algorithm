a0f779fc589182a28e5a3990c7754f6dc89fa3c1
qkrgks456 <6987410@naver.com>
2021-12-27 18:21:54 +0900

    포도주 시식에서 막힘 망함

--- src/baekjoon/dp1/Main10.java
+++ src/baekjoon/dp1/Main10.java
@@ -1,7 +1,19 @@
 package baekjoon.dp1;
 
+import java.util.Scanner;
+
 public class Main10 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        int n = scanner.nextInt();
+        int[] ints = new int[n];
+        int[] p = new int[n];
+        int[] g = new int[n];
+        for (int i = 0; i < n; i++) {
+            ints[i] = scanner.nextInt();
+        }
 
+        System.out.println(max);
     }
 }
