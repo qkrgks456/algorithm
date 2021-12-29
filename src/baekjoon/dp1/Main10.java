51565dac676b1ef4d1705971da992d25a51217da
qkrgks456 <6987410@naver.com>
2021-12-29 09:23:25 +0900

    병합 해결

--- src/baekjoon/dp1/Main10.java
+++ src/baekjoon/dp1/Main10.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class Main10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int max = Integer.MIN_VALUE;
         int n = scanner.nextInt();
-        int[] ints = new int[n];
-        int[] p = new int[n];
-        int[] g = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] ints = new int[n + 1];
+        int[] ch = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             ints[i] = scanner.nextInt();
         }
+        ch[1] = ints[1];
+        if (n != 1) {
+            ch[2] = ints[1] + ints[2];
+        }
+        for (int i = 3; i < n; i++) {
+            ch[i] = Math.max(Math.max(ch[i - 1], ch[i - 3] + ints[i - 1] + ints[i]), ch[i - 2] + ints[i]);
+        }
+        if (n != 1) {
+            System.out.println(ch[n]);
+        } else {
+            System.out.println(ch[1]);
+        }
 
-        System.out.println(max);
     }
 }
