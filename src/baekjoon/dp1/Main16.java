dddf3539eb5999015d318e07ca221d69542f6faa
qkrgks456 <6987410@naver.com>
2021-12-31 14:11:40 +0900

    배낭합 마무리

--- src/baekjoon/dp1/Main16.java
+++ src/baekjoon/dp1/Main16.java
@@ -1,7 +1,19 @@
 package baekjoon.dp1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main16 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[][] ints = new int[n][2];
+        int[][] ch = new int[n][k];
+        for (int i = 0; i < n; i++) {
+            ints[i][0] = scanner.nextInt();
+            ints[i][1] = scanner.nextInt();
+        }
 
     }
 }
