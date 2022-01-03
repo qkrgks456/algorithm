5688be80ca5ab76de868e932d7bda5b0ebc763f3
qkrgks456 <6987410@naver.com>
2022-01-03 17:28:51 +0900

    와 정수론 진짜 어디까지 풀어재끼는 거니 골드 빼고는 다 할만했네 수학 재밌다

---
+++ src/baekjoon/intCombi/Main10.java
@@ -0,0 +1,13 @@
+package baekjoon.intCombi;
+
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
