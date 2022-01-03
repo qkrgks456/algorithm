dcb68e19f771e9528f5aa2a86825df65079a51a3
qkrgks456 <6987410@naver.com>
2022-01-03 18:24:14 +0900

    아 왜 여기서 막히냐 ㅡ.ㅡ

--- src/baekjoon/intCombi/Main10.java
+++ src/baekjoon/intCombi/Main10.java
@@ -7,7 +7,15 @@ public class Main10 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         for (int i = 0; i < n; i++) {
-
+            int n1 = scanner.nextInt();
+            String[][] strings = new String[n1][2];
+            for (int j = 0; j < n1; j++) {
+                String s = scanner.nextLine();
+                String[] strings1 = s.split(" ");
+                strings[j][0] = strings1[0];
+                strings[j][1] = strings1[1];
+            }
+            // 여기서 부터 하면되는데 ..
         }
     }
 }
