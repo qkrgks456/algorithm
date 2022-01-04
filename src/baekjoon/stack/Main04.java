8eaf9b9a16eeb0966e17310b2942d8e032bc3a93
qkrgks456 <6987410@naver.com>
2022-01-04 16:46:25 +0900

    스택은 역시 재밌다 하하하

---
+++ src/baekjoon/stack/Main04.java
@@ -0,0 +1,20 @@
+package baekjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main04 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            Stack<String> stack = new Stack<>();
+            String s = bufferedReader.readLine();
+            if (s.equals(".")) {
+                break;
+            }
+            String[] strings = s.split("");
+
+        }
+    }
+}
