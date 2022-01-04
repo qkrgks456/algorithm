d335f32663301d66da1d843c9fecef0938c23a98
qkrgks456 <6987410@naver.com>
2022-01-04 16:46:25 +0900

    스택은 역시 재밌다 하하하

---
+++ src/baekjoon/stack/Main02.java
@@ -0,0 +1,25 @@
+package baekjoon.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        int result = 0;
+        while (!stack.empty()) {
+            result += stack.pop();
+        }
+        System.out.println(result);
+    }
+}
