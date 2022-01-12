7bad6ce4e88f51899f6bcc5c448cc95fc8a4055a
qkrgks456 <6987410@naver.com>
2022-01-12 17:20:21 +0900

    골1문제는 역대급 ㅋㅋ 곱셈의 역원 페르마의 소정리는 살면서 처음봤다

---
+++ src/baekjoon/divideConquer/Main04.java
@@ -0,0 +1,32 @@
+package baekjoon.divideConquer;
+
+import java.util.Scanner;
+
+public class Main04 {
+    // 2147483647
+    // 이러면 속도초과 날것 같은데 시부러 ..
+    // 배열에 담자니 21억개 배열 만드는것도 이상하고 ㅋㅋ
+    // 모듈러 연산 개 빡치네 ㅋㅋㅋ
+    // 나머지 정리 잘 못해서 자꼬 틀리네
+    static long a = 0;
+    static long c = 0;
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        long b = scanner.nextInt();
+        c = scanner.nextInt();
+        System.out.println(DFS(b));
+    }
+
+    public static long DFS(long b) {
+        if (b == 1) {
+            return a % c;
+        }
+        long check = DFS(b / 2);
+        if (b % 2 == 1) {
+            return (check * check % c) * a % c;
+        }
+        return check * check % c;
+    }
+}
---
+++ src/baekjoon/divideConquer/Main05.java
@@ -0,0 +1,41 @@
+package baekjoon.divideConquer;
+
+import java.util.Scanner;
+
+public class Main05 {
+    // 최대 4000000
+    // 곱셈의 역원을 어떻게 구해야 하는지 전혀 모르겠다
+    // 이걸보고 조졌다고 한다 ㅋㅋㅋ
+    // 와 골1문제 레전드네 개 어렵다 진짜
+    static long num = 1000000007;
+    static long[] longs = new long[4000001];
+    static long bc;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        longs[0] = 1;
+        longs[1] = 1;
+        for (int i = 2; i < longs.length; i++) {
+            longs[i] = longs[i - 1] * i % num;
+        }
+        // 여기식이 제일 어려움
+        // 페르마의 소정리 증명하는 방법이 너무 어려움
+        long a = longs[n];
+        bc = longs[k] * longs[n - k] % num;
+        System.out.println(a * DFS(num - 2) % num);
+    }
+
+    // 지수 분할 정복하는거 이건 전에 해봐서 할만했음
+    public static long DFS(long n2) {
+        if (n2 == 1) {
+            return bc % num;
+        }
+        long check = DFS(n2 / 2);
+        if (n2 % 2 == 1) {
+            return (check * check % num) * bc % num;
+        }
+        return check * check % num;
+    }
+}
