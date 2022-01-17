b6db7dca29e14c44c1011533e1fdb97e58d02dad
qkrgks456 <6987410@naver.com>
2022-01-17 09:34:49 +0900

    병합 해결

--- src/baekjoon/divideConquer/Main06.java
+++ src/baekjoon/divideConquer/Main06.java
@@ -3,10 +3,13 @@ package baekjoon.divideConquer;
 import java.util.Scanner;
 
 public class Main06 {
+    static int[][] first;
+    static int[][] second;
+    static int[][] result;
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        int[][] first;
-        int[][] second;
+
         for (int i = 0; i < 2; i++) {
             int n = scanner.nextInt();
             int n1 = scanner.nextInt();
@@ -26,5 +29,11 @@ public class Main06 {
                 }
             }
         }
+        DFS(0, 0);
+    }
+
+    public static int DFS(int x, int y) {
+
+        return DFS(x, y) * DFS(x, y);
     }
 }
