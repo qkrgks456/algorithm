2d5339bf7c79a44f30c55918e9c71467deb65fb9
qkrgks456 <6987410@naver.com>
2022-01-20 18:13:47 +0900

    기본적인 증가수열은 확실하게 이해했는데

--- src/baekjoon/binarySearch/Main07.java
+++ src/baekjoon/binarySearch/Main07.java
@@ -9,17 +9,31 @@ public class Main07 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
         int[] ints = new int[n];
+        int[] check = new int[n];
         String s = bufferedReader.readLine();
         String[] strings = s.split(" ");
         for (int i = 0; i < n; i++) {
             ints[i] = Integer.parseInt(strings[i]);
         }
         // 100만이면 시간 그냥 터져 버리쥬 이분탐색을 활용해라 ?
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < ints.length; i++) {
+            check[i] = 1;
+            for (int j = 0; j < ints.length; j++) {
+                if (ints[i] > ints[j] && check[i] < check[j] + 1) {
+                    check[i] += 1;
+                }
+            }
+            result = Math.max(result, check[i]);
+        }
+
         int l = 1;
         int r = n + 1;
         while (l < r) {
             int mid = (l + r) / 2;
 
         }
+
+
     }
 }
