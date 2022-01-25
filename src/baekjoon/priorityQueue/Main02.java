3c0876ac9f0423acb3fa42560ace9b9352755158
qkrgks456 <6987410@naver.com>
2022-01-25 13:14:08 +0900

    최소 힙 끝

--- src/baekjoon/priorityQueue/Main02.java
+++ src/baekjoon/priorityQueue/Main02.java
@@ -2,19 +2,21 @@ package baekjoon.priorityQueue;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main02 {
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
-        MaxHeap maxHeap = new MaxHeap();
+        MinHeap minHeap = new MinHeap();
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(bufferedReader.readLine());
             if (num == 0) {
-                stringBuilder.append(maxHeap.delete() + "\n");
+                stringBuilder.append(minHeap.delete() + "\n");
             } else {
-                maxHeap.insert(num);
+                minHeap.insert(num);
             }
         }
         System.out.println(stringBuilder);
@@ -22,5 +24,48 @@ public class Main02 {
 }
 
 class MinHeap {
+    public List<Integer> list;
 
+    public MinHeap() {
+        list = new ArrayList<>();
+        list.add(0);
+    }
+
+    public void insert(int num) {
+        list.add(num);
+        int idx = list.size() - 1;
+        while (idx > 1 && list.get(idx) < list.get(idx / 2)) {
+            swap(idx / 2, idx);
+            idx /= 2;
+        }
+    }
+
+    public int delete() {
+        if (list.size() == 1) {
+            return 0;
+        }
+        int deleteNum = list.get(1);
+        list.set(1, list.get(list.size() - 1));
+        list.remove(list.size() - 1);
+        int idx = 1;
+        while (idx * 2 < list.size()) {
+            int checkIdx = idx * 2;
+            if (idx * 2 + 1 < list.size() && list.get(idx * 2) > list.get(idx * 2 + 1)) {
+                checkIdx = idx * 2 + 1;
+            }
+            if (list.get(idx) < list.get(checkIdx)) {
+                break;
+            } else {
+                swap(idx, checkIdx);
+                idx = checkIdx;
+            }
+        }
+        return deleteNum;
+    }
+
+    public void swap(int idx1, int idx2) {
+        int n = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, n);
+    }
 }
