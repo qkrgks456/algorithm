8894de7c987ef6418fb075cd70275df3668b84c1
parkhan <6987410@naver.com>
2022-02-04 12:14:50 +0900

    추가된 애들인데 이게 뭐냐 ㅋㅋ

---
+++ src/baekjoon/input/Main10.java
@@ -0,0 +1,12 @@
+package baekjoon.input;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main10 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        System.out.println(s += "??!");
+    }
+}
