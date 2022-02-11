d3c4a2a86bde1a50c7fe65bb93b41ddb0618beb0
qkrgks456 <6987410@naver.com>
2022-02-11 18:11:07 +0900

    문제도 개 어렵네 ㅋㅋ

---
+++ src/baekjoon/dfsbfs/Main11.java
@@ -0,0 +1,14 @@
+package baekjoon.dfsbfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main11 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
