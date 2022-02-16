d4ea9d2a7ac31fd48ab69e328d870b965ae9eed6
qkrgks456 <6987410@naver.com>
2022-02-16 18:07:36 +0900

    골3 경축 ~

--- src/baekjoon/dp1/Main9095.java
+++ src/baekjoon/dp1/Main9095.java
@@ -8,12 +8,14 @@ public class Main9095 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         ints[1] = 1;
-        for (int i = 2; i < ints.length; i++) {
-            ints[i] = ints[i - 1] + i - 1;
+        ints[2] = 2;
+        ints[3] = 4;
+        for (int i = 4; i < ints.length; i++) {
+            ints[i] = ints[i - 1] + ints[i - 2] + ints[i - 3];
         }
         for (int i = 0; i < n; i++) {
             int num = scanner.nextInt();
-
+            System.out.println(ints[num]);
         }
     }
 }
