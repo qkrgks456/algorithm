94e729393be13d3b9b8fdd872ec27d5e8ad43a84
parkhan <6987410@naver.com>
2022-02-19 18:05:46 +0900

    골4 맞음 ?? 온도차 지리네

--- src/baekjoon/etc/Main1987.java
+++ src/baekjoon/etc/Main1987.java
@@ -1,14 +1,11 @@
 package baekjoon.etc;
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main1987 {
-    static int r, c;
+    static int r, c, max;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static char[][] graph;
@@ -21,32 +18,28 @@ public class Main1987 {
         r = Integer.parseInt(stringTokenizer.nextToken());
         c = Integer.parseInt(stringTokenizer.nextToken());
         graph = new char[r][c];
+        result = new int[r][c];
         for (int i = 0; i < r; i++) {
             String[] strings = bufferedReader.readLine().split("");
             for (int j = 0; j < c; j++) {
                 graph[i][j] = strings[j].charAt(0);
             }
         }
-        System.out.println('z' - 97);
+        ch2[graph[0][0] - 65] = 1;
+        DFS(0, 0, 1);
+        System.out.println(max);
     }
 
-    public static void BFS() {
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(0, 0));
-        result[0][0] = 1;
-        ch2[graph[0][0] - 97] = 1;
+    public static void DFS(int x, int y, int count) {
+        max = Math.max(max, count);
         int nx, ny;
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                nx = point.x + dx[i];
-                ny = point.y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < r && ny < c) {
-                    if (ch2[graph[nx][ny] - 97] == 0) {
-                        ch2[graph[nx][ny] - 97] = 1;
-
-                    }
-                }
+        for (int i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < r && ny < c && ch2[graph[nx][ny] - 65] == 0) {
+                ch2[graph[nx][ny] - 65] = 1;
+                DFS(nx, ny, count + 1);
+                ch2[graph[nx][ny] - 65] = 0;
             }
         }
     }
