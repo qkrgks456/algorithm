d41113dbb05bee2263423a6224539e12b2c6bc6a
qkrgks456 <6987410@naver.com>
2022-02-23 12:53:14 +0900

    약간 하드코딩 느낌인데 괜찮았다 ?

--- src/baekjoon/etc/Main1759.java
+++ src/baekjoon/etc/Main1759.java
@@ -9,7 +9,6 @@ public class Main1759 {
     static int l, c;
     static char[] result, chars;
     static int[] ch = new int[26];
-    static int[] ch2 = new int[26];
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -28,6 +27,18 @@ public class Main1759 {
 
     public static void recursion(int n) {
         if (n == l) {
+            int count1 = 0;
+            int count2 = 0;
+            for (char c : result) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    count1++;
+                } else {
+                    count2++;
+                }
+            }
+            if (count1 < 1 || count2 < 2) {
+                return;
+            }
             for (char c : result) {
                 System.out.print(c);
             }
@@ -37,8 +48,11 @@ public class Main1759 {
         // +0 , +1, +2
         for (int i = n; i < c; i++) {
             if (ch[chars[i] - 97] == 0) {
-                ch[chars[i] - 97] = 1;
                 result[n] = chars[i];
+                if (n != 0) {
+                    if (result[n - 1] > result[n]) continue;
+                }
+                ch[chars[i] - 97] = 1;
                 recursion(n + 1);
                 ch[chars[i] - 97] = 0;
             }
