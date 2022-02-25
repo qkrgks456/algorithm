fd1caace89bb71dde7fece5a21add238a6c17757
qkrgks456 <6987410@naver.com>
2022-02-25 17:24:42 +0900

    뭐냐 24498번 저게 어케 실버4임 ㅋㅋ 조건때문에 사람들 많이 틀릴듯

--- src/inflearn/dfsbfsUpgrade/Main12.java
+++ src/inflearn/dfsbfsUpgrade/Main12.java
@@ -1,22 +1,55 @@
 package inflearn.dfsbfsUpgrade;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main12 {
-    static int[] dx = {1, -1, 0, 0, -1, 1};
-    static int[] dy = {0, 0, 1, -1, -1, 1};
+    static int[] dx = {1, -1, 0, 0, -1, 1, 1, -1};
+    static int[] dy = {0, 0, 1, -1, -1, 1, -1, 1};
+    static int[][] ints, ch;
+    static int n;
+    static int result = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        int[][] ints = new int[n][n];
+        n = Integer.parseInt(bufferedReader.readLine());
+        ints = new int[n][n];
+        ch = new int[n][n];
         for (int i = 0; i < n; i++) {
             String[] strings = bufferedReader.readLine().split(" ");
             for (int j = 0; j < n; j++) {
                 ints[i][j] = Integer.parseInt(strings[j]);
             }
         }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (ints[i][j] == 1 && ch[i][j] == 0) {
+                    result++;
+                    BFS(i, j);
+                }
+            }
+        }
+        System.out.println(result);
     }
 
+    public static void BFS(int x, int y) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(x, y));
+        ch[x][y] = 1;
+        int nx, ny;
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            for (int i = 0; i < 8; i++) {
+                nx = point.x + dx[i];
+                ny = point.y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && ch[nx][ny] == 0 && ints[nx][ny] == 1) {
+                    ch[nx][ny] = 1;
+                    queue.add(new Point(nx, ny));
+                }
+            }
+        }
+    }
 }
