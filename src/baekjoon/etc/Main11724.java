59a554ab63eae022bf5c8c069695b6028c736b50
박한솔 <6987410@naver.com>
2022-03-07 18:22:47 +0900

    아주 열정적인 문제풀이 좋았다

--- src/baekjoon/etc/Main11724.java
+++ src/baekjoon/etc/Main11724.java
@@ -10,13 +10,14 @@ import java.util.Queue;
 public class Main11724 {
     static int[] ch;
     static List<ArrayList<Integer>> list = new ArrayList<>();
+    static int result = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String[] strings = bufferedReader.readLine().split(" ");
         int n = Integer.parseInt(strings[0]);
         int m = Integer.parseInt(strings[1]);
-        ch = new int[n];
+        ch = new int[n + 1];
         for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
@@ -27,19 +28,25 @@ public class Main11724 {
         }
         for (int i = 1; i <= n; i++) {
             if (ch[i] == 0) {
+                result++;
                 BFS(i);
             }
         }
+        System.out.println(result);
     }
 
     public static void BFS(int num) {
         Queue<Integer> queue = new LinkedList<>();
+        queue.add(num);
         ch[num] = 1;
-        for (int n : list.get(num)) {
-            queue.add(n);
-        }
         while (!queue.isEmpty()) {
-            queue.poll();
+            int number = queue.poll();
+            for (int n : list.get(number)) {
+                if (ch[n] == 0) {
+                    ch[n] = 1;
+                    queue.add(n);
+                }
+            }
         }
     }
 }
