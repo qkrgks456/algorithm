904c661142973654064147290a4048ea6dac9972
박한솔 <6987410@naver.com>
2022-03-07 18:22:47 +0900

    아주 열정적인 문제풀이 좋았다

---
+++ src/baekjoon/etc/Main1764.java
@@ -0,0 +1,29 @@
+package baekjoon.etc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main1764 {
+    public static void main(String[] args) throws Exception {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(1,3);
+        /*System.out.println(map.get());*/
+        /*BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = bufferedReader.readLine().split(" ");
+        HashMap<String, Integer> map = new HashMap<>();
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
+        for (int i = 0; i < n; i++) {
+            map.put(bufferedReader.readLine(), 1);
+        }
+        for (int i = 0; i < m; i++) {
+            String s = bufferedReader.readLine();
+            if (map.get(s) != null) {
+
+            }
+        }*/
+
+
+    }
+}
