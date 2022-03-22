16cb28eb781a3985da2b71e52a7d4120a6a58b9f
박한솔 <6987410@naver.com>
2022-03-22 14:56:38 +0900

    bfs 썩세스

---
+++ src/baekjoon/etc/Main14503.java
@@ -0,0 +1,14 @@
+package baekjoon.etc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main14503 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = bufferedReader.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
+
+    }
+}
