d79d8c78824b199fe791c79fcb026f65ac9d7e19
박한솔 <6987410@pineone.com>
2022-03-22 13:04:38 +0900

    바빠서 문제 풀 시간이 없네 ㅡ.ㅡ 정렬 썩세스

--- src/baekjoon/etc/Main1026.java
+++ src/baekjoon/etc/Main1026.java
@@ -2,18 +2,14 @@ package baekjoon.etc;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main1026 {
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
         int[] first = new int[n];
-        int[] second = new int[n];
-        int[] result = new int[n];
+        Integer[] second = new Integer[n];
         String[] strings = bufferedReader.readLine().split(" ");
         String[] strings1 = bufferedReader.readLine().split(" ");
         for (int i = 0; i < n; i++) {
@@ -21,8 +17,11 @@ public class Main1026 {
             second[i] = Integer.parseInt(strings1[i]);
         }
         Arrays.sort(first);
-        for (int i = n - 1; i >= 0; i--) {
-            System.out.print(first[i] + " ");
+        Arrays.sort(second, Collections.reverseOrder());
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += second[i] * first[i];
         }
+        System.out.println(result);
     }
 }
