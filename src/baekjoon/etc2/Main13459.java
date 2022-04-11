25a931fc8d83f2a2fbfac0cc1c9247c416ce5ebf
박한솔 <6987410@naver.com>
2022-04-11 16:28:20 +0900

    여기는 생지옥 이었습니다 ^오^

--- src/baekjoon/etc2/Main13459.java
+++ src/baekjoon/etc2/Main13459.java
@@ -1,6 +1,5 @@
 package baekjoon.etc2;
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
@@ -8,13 +7,12 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main13459 {
-    static int N, M, count;
+    static int N, M;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
-    static Point arrive, B;
     static char[][] chars;
-    static int[][] ch;
-    static Queue<Point> queue = new LinkedList<>();
+    static int[][][][] ch;
+    static Queue<Boll> queue = new LinkedList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -22,77 +20,141 @@ public class Main13459 {
         N = Integer.parseInt(stringTokenizer.nextToken());
         M = Integer.parseInt(stringTokenizer.nextToken());
         chars = new char[N][M];
-        ch = new int[N][M];
+        ch = new int[N][M][N][M];
+        int a = 0, b = 0, c = 0, d = 0;
         for (int i = 0; i < N; i++) {
             String s = bufferedReader.readLine();
             for (int j = 0; j < M; j++) {
                 chars[i][j] = s.charAt(j);
-                if (chars[i][j] == 'O') {
-                    arrive = new Point(i, j);
-                } else if (chars[i][j] == 'R') {
-                    ch[i][j] = 2;
-                    queue.add(new Point(i, j));
+                if (chars[i][j] == 'R') {
+                    a = i;
+                    b = j;
                 } else if (chars[i][j] == 'B') {
-                    ch[i][j] = 2;
-                    B = new Point(i, j);
-                } else if (chars[i][j] == '#') {
-                    ch[i][j] = 1;
+                    c = i;
+                    d = j;
                 }
             }
         }
+        queue.add(new Boll(a, b, c, d));
+        ch[a][b][c][d] = 1;
+        BFS();
     }
 
     public static void BFS() {
-        int nx, ny;
+        int count = 0;
         while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                nx = point.x + dx[i];
-                ny = point.y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M && ch[nx][ny] == 0) {
-                    if (i == 0) {
-                        // 위
-                        ch[point.x][point.y] = 0;
-                        while (ch[nx][ny] < 1) {
-                            if (chars[nx][ny] == 'O') {
-                                System.exit(0);
-                            }
-                            nx--;
-                        }
-                        queue.add(new Point(nx + 1, ny));
-                    } else if (i == 1) {
-                        // 왼쪽
-                        ch[point.x][point.y] = 0;
-                        while (ch[nx][ny] < 1) {
-                            if (chars[nx][ny] == 'O') {
-                                System.exit(0);
-                            }
-                            ny--;
-                        }
-                        queue.add(new Point(nx, ny + 1));
-                    } else if (i == 2) {
-                        // 아래
-                        ch[point.x][point.y] = 0;
-                        while (ch[nx][ny] < 1) {
-                            if (chars[nx][ny] == 'O') {
-                                System.exit(0);
-                            }
-                            nx++;
-                        }
-                        queue.add(new Point(nx - 1, ny));
-                    } else {
-                        // 오른쪽
-                        ch[point.x][point.y] = 0;
-                        while (ch[nx][ny] < 1) {
-                            if (chars[nx][ny] == 'O') {
-                                System.exit(0);
-                            }
-                            ny++;
-                        }
-                        queue.add(new Point(nx, ny - 1));
-                    }
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Boll boll = queue.poll();
+                for (int j = 0; j < 4; j++) {
+                    move(boll.rx, boll.ry, boll.bx, boll.by, j);
                 }
             }
+            count++;
+            if (count == 10) {
+                System.out.println(0);
+                System.exit(0);
+            }
+        }
+        System.out.println(0);
+    }
+
+    public static void move(int rx, int ry, int bx, int by, int num) {
+        // 일단 빨간놈이 먼전지 파란놈이 먼전지 체크 만약이 같은자리가 아니면 어차피 이거 상관없음
+        boolean redFirst = false;
+        if (num == 0 && rx > bx) redFirst = true;
+        if (num == 1 && ry < by) redFirst = true;
+        if (num == 2 && rx < bx) redFirst = true;
+        if (num == 3 && ry > by) redFirst = true;
+        // 빨간놈부터 돌려
+        int nx = rx, ny = ry;
+        while (true) {
+            nx += dx[num];
+            ny += dy[num];
+            if (chars[nx][ny] == '#') {
+                break;
+            }
+            rx = nx;
+            ry = ny;
+            if (chars[nx][ny] == 'O') {
+                break;
+            }
+        }
+
+        // 퍼런놈 돌려
+        nx = bx;
+        ny = by;
+        while (true) {
+            nx += dx[num];
+            ny += dy[num];
+            if (chars[nx][ny] == '#') {
+                break;
+            }
+            bx = nx;
+            by = ny;
+            if (chars[nx][ny] == 'O') {
+                break;
+            }
+        }
+        if (chars[bx][by] == 'O') {
+            return;
+        }
+        // 아얘 겹치면 어떻게 되는가
+        if (rx == bx && ry == by) {
+            switch (num) {
+                case 0:
+                    if (redFirst) {
+                        bx--;
+                    } else {
+                        rx--;
+                    }
+                    break;
+                case 1:
+                    if (redFirst) {
+                        by++;
+                    } else {
+                        ry++;
+                    }
+                    break;
+                case 2:
+                    if (redFirst) {
+                        bx++;
+                    } else {
+                        rx++;
+                    }
+                    break;
+                case 3:
+                    if (redFirst) {
+                        by--;
+                    } else {
+                        ry--;
+                    }
+                    break;
+            }
+        }
+        if (chars[rx][ry] == 'O') {
+            System.out.println(1);
+            System.exit(0);
+        }
+
+        if (ch[rx][ry][bx][by] == 0) {
+            ch[rx][ry][bx][by] = 1;
+            queue.add(new Boll(rx, ry, bx, by));
         }
     }
+}
+
+class Boll {
+    int rx;
+    int ry;
+    int bx;
+    int by;
+
+    public Boll(int rx, int ry, int bx, int by) {
+        this.rx = rx;
+        this.ry = ry;
+        this.bx = bx;
+        this.by = by;
+    }
+
 }
