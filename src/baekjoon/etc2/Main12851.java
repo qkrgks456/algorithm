ea13f6a88f7a96dca4f36e87d9c1c8c0ed3d3788
박한솔 <6987410@naver.com>
2022-04-12 09:50:04 +0900

    약간 찝찝한 승리

--- src/baekjoon/etc2/Main12851.java
+++ src/baekjoon/etc2/Main12851.java
@@ -8,16 +8,15 @@ import java.util.StringTokenizer;
 
 public class Main12851 {
     static int N, K;
-    static int check = 0;
+    static int check = Integer.MAX_VALUE;
     static int count = 0;
-    static int[] ch, result;
+    static int[] result;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         N = Integer.parseInt(stringTokenizer.nextToken());
         K = Integer.parseInt(stringTokenizer.nextToken());
-        ch = new int[100001];
         result = new int[100001];
         if (N == K) {
             System.out.println(0);
@@ -26,6 +25,7 @@ public class Main12851 {
         }
         BFS();
         System.out.println(check);
+        System.out.println(count);
     }
 
     // 아 방법수 구하는거 빡도네 쉬운듯 보이면서 짱구 굴려야되네
@@ -34,33 +34,34 @@ public class Main12851 {
         queue.add(N);
         while (!queue.isEmpty()) {
             Integer num = queue.poll();
-            if (num == K) {
+            if (result[num] > check) return;
+            if (num == K && result[num] <= check) {
                 check = result[num];
+                count++;
             }
+
             int n1 = num - 1;
             int n2 = num + 1;
             int n3 = num * 2;
             if (n1 >= 0) {
-                if (ch[n1] == 0) {
-                    ch[n1] = 1;
+                if (result[n1] == 0 || result[n1] >= result[num] + 1) {
                     result[n1] = result[num] + 1;
                     queue.add(n1);
                 }
             }
             if (n2 <= 100000) {
-                if (ch[n2] == 0) {
-                    ch[n2] = 1;
+                if (result[n2] == 0 || result[n2] >= result[num] + 1) {
                     result[n2] = result[num] + 1;
                     queue.add(n2);
                 }
             }
             if (n3 <= 100001) {
-                if (ch[n3] == 0) {
-                    ch[n3] = 1;
+                if (result[n3] == 0 || result[n3] >= result[num] + 1) {
                     result[n3] = result[num] + 1;
                     queue.add(n3);
                 }
             }
+
         }
     }
 }
