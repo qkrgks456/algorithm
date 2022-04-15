ac5a052ad74487b6a26688090d2b4b66b44ed85d
박한솔 <6987410@naver.com>
2022-04-15 13:24:19 +0900

    골2 달성 축하합니다. 이제 보상으로 잠시 휴식

--- src/baekjoon/etc3/Main1713.java
+++ src/baekjoon/etc3/Main1713.java
@@ -5,88 +5,61 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main1713 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
         int M = Integer.parseInt(bufferedReader.readLine());
-        int[] member = new int[M];
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < M; i++) {
-            member[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
         List<Member> list = new ArrayList<>();
         for (int i = 0; i < M; i++) {
-            if (list.size() == N) {
-                boolean checks = true;
-                // 최소값 구하기
-                int min = Integer.MAX_VALUE;
-                for (int j = 0; j < list.size(); j++) {
-                    if (min > list.get(j).count) {
-                        min = list.get(j).count;
-                    }
-                }
-                // 같은값일경우 실행 안함
-                if (overlap(list, member[i])) {
-                    // 최소값 2개이상이지 확인
-                    int check = 0;
-                    int idx = 0;
-                    for (int j = 0; j < list.size(); j++) {
-                        if (min == list.get(j).count) {
-                            idx = j;
-                            check++;
-                        }
-                    }
-                    // 오래된놈 제거 여기서 망했네 자료구조 엎어야한다 ㅋㅋ
-                    if (check >= 2) {
-                        list.remove(0);
-                        list.add(new Member(member[i], 1));
-                    } else {
-                        // 최소값제거
-                        list.remove(idx);
-                        list.add(idx, new Member(member[i], 1));
-                    }
-                }
-            } else {
-                // 중복시 실행 안함
-                if (overlap(list, member[i])) {
-                    list.add(new Member(member[i], 1));
+            int member = Integer.parseInt(stringTokenizer.nextToken());
+            // 중복이 안됐으면
+            boolean checks = false;
+            for (Member members : list) {
+                // 같은값이면 플러스
+                if (member == members.num) {
+                    checks = true;
+                    members.count++;
+                    break;
                 }
             }
+            // 중복시 건너띔
+            if (!checks) {
+                // 사이즈 꽉 찼을떄
+                if (list.size() == N) {
+                    // 최소값중에 오래된놈 혹은 최소값 제거
+                    list.remove(0);
+                    list.add(new Member(member, 1, i));
+                } else {
+                    // 사이즈가 남을때
+                    list.add(new Member(member, 1, i));
+                }
+            }
+            Collections.sort(list);
         }
+        Collections.sort(list, (o1, o2) -> o1.num - o2.num);
         // 마무리 정렬까지
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i).num;
+        for (Member members : list) {
+            System.out.print(members.num + " ");
         }
-        Arrays.sort(result);
-        for (int n : result) {
-            System.out.print(n + " ");
-        }
-    }
-
-    // 중복체크
-    public static boolean overlap(List<Member> list, int memberNum) {
-        boolean checks = true;
-        for (int j = 0; j < list.size(); j++) {
-            // 같은값이면 플러스
-            if (memberNum == list.get(j).num) {
-                checks = false;
-                int count = list.get(j).count;
-                int num = list.get(j).num;
-                list.remove(j);
-                list.add(new Member(num, count + 1));
-            }
-        }
-        return checks;
     }
 }
 
-class Member {
-    int num;
-    int count;
+class Member implements Comparable<Member> {
+    int num, count, day;
 
-    public Member(int num, int count) {
+    public Member(int num, int count, int day) {
         this.num = num;
         this.count = count;
+        this.day = day;
+    }
+
+    @Override
+    public int compareTo(Member o) {
+        if (this.count == o.count) {
+            return this.day - o.day;
+        }
+        return this.count - o.count;
     }
 }
