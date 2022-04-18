0a6cf849790088ba0fd3aae819c623c5d6bef947
박한솔 <6987410@naver.com>
2022-04-18 10:24:20 +0900

    dp 폐관수련 스타트

---
+++ src/baekjoon/etc3/Test.java
@@ -0,0 +1,11 @@
+package baekjoon.etc3;
+
+public class Test {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= 1024; i++) {
+            stringBuilder.append("아");
+        }
+        System.out.println(stringBuilder);
+    }
+}
