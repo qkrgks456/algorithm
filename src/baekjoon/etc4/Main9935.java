42331acbb228321bee3e90cad188e613b7aaa8ce
박한솔 <6987410@pineone.com>
2022-04-25 17:58:48 +0900

    업무를 위해 잠시 보류

---
+++ src/baekjoon/etc4/Main9935.java
@@ -0,0 +1,21 @@
+package baekjoon.etc4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main9935 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        String s2 = bufferedReader.readLine();
+        char[] chars = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            chars[i] = s.charAt(i);
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < chars.length; i++) {
+
+        }
+    }
+}
