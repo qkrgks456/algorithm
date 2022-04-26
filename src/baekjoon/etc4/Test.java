0f1ba34f6d194d147092673d97d31e433054faf1
박한솔 <6987410@naver.com>
2022-04-26 17:45:18 +0900

    왜 이렇게 귀찮게 하는거야 ㅡ.ㅡ

--- src/baekjoon/etc4/Test.java
+++ src/baekjoon/etc4/Test.java
@@ -1,13 +1,20 @@
 package baekjoon.etc4;
 
-public class Test {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-        // 3.3333333333333
-        double num = (double) a / b;
-        // 3.33 / 100.0
-        System.out.println("10 / 3 = " + Math.round(num * 100) / 100.0);
+public class Test {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int n1 = Integer.parseInt(stringTokenizer.nextToken());
+            int n2 = Integer.parseInt(stringTokenizer.nextToken());
+            stringBuilder.append("Case #" + (i + 1) + ": " + n1 + " + " + n2 + " = " + (n1 + n2) + "\n");
+        }
+        System.out.println(stringBuilder);
     }
 }
