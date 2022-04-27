36fd0b7d06dc7df272ad674807234561efbb5fd1
박한솔 <6987410@naver.com>
2022-04-27 14:15:52 +0900

    클래스 문제 다 풀기 2완료 3시작

--- src/baekjoon/etc4/Main18111.java
+++ src/baekjoon/etc4/Main18111.java
@@ -23,13 +23,31 @@ public class Main18111 {
             }
         }
         // 어차피 최대치 넘거나, 최소치 아래면 최소시간이 나올수 없음
+        int result = Integer.MAX_VALUE;
+        int height = 0;
         for (int i = min; i <= max; i++) {
+            int time = 0;
+            int check = B;
+            // 이게 한 플로우
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < M; k++) {
-
+                    if (ints[j][k] > i) {
+                        // 크다면 2초로
+                        time += (ints[j][k] - i) * 2;
+                        check += ints[j][k] - i;
+                    } else if (ints[j][k] < i) {
+                        time += i - ints[j][k];
+                        check -= i - ints[j][k];
+                    }
+                }
+            }
+            if (check >= 0) {
+                if (result >= time) {
+                    result = time;
+                    height = i;
                 }
             }
         }
-
+        System.out.println(result + " " + height);
     }
 }
