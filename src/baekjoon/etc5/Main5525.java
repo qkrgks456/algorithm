5ee4dbeb70f2f2d3d97a891f2894f1513598d7f3
박한솔 <6987410@pineone.com>
2022-04-29 11:11:53 +0900

    느슨한걸

---
+++ src/baekjoon/etc5/Main5525.java
@@ -0,0 +1,15 @@
+package baekjoon.etc5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main5525 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int M = Integer.parseInt(bufferedReader.readLine());
+        String s = bufferedReader.readLine();
+
+
+    }
+}
---
+++ src/baekjoon/etc5/Test.java
@@ -0,0 +1,7 @@
+package baekjoon.etc5;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
