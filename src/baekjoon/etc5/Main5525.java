706ee9e1b17e5d3fee4e62e696be2564f8c8b0c2
박한솔 <6987410@pineone.com>
2022-04-29 16:43:02 +0900

    아니 생각보다 어려운문제인데 ?

--- src/baekjoon/etc5/Main5525.java
+++ src/baekjoon/etc5/Main5525.java
@@ -5,11 +5,25 @@ import java.io.InputStreamReader;
 
 public class Main5525 {
     public static void main(String[] args) throws Exception {
+        // 어려운데 ??
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
         int M = Integer.parseInt(bufferedReader.readLine());
-        String s = bufferedReader.readLine();
-
-
+        char[] chars = bufferedReader.readLine().toCharArray();
+        int count = 0;
+        int result = 0;
+        for (int i = 1; i < M - 1; i++) {
+            if (chars[i - 1] == 'I' && chars[i] == 'O' && chars[i + 1] == 'I') {
+                count++;
+                if (count == N) {
+                    count--;
+                    result++;
+                }
+                i++;
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println(result);
     }
 }
