5d8693fe2574cb199263abe14ff5aeb3271e7ff2
박한솔 <6987410@naver.com>
2022-05-02 18:14:36 +0900

    아 티어 낮은데 왜 이렇게 어려워 ㅡ.ㅡ

--- src/baekjoon/etc5/Test.java
+++ src/baekjoon/etc5/Test.java
@@ -2,8 +2,6 @@ package baekjoon.etc5;
 
 public class Test {
     public static void main(String[] args) {
-        String s = "안녕하세요";
-
-        System.out.println(s.substring(1));
+        System.out.println((int) Math.sqrt(1));
     }
 }
