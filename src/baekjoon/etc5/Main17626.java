e3948d1183573d1e961734ad90f0f7fcaec7e6fb
박한솔 <6987410@pineone.com>
2022-05-03 15:31:01 +0900

    아 왜 이렇게 해매는것이야

--- src/baekjoon/etc5/Main17626.java
+++ src/baekjoon/etc5/Main17626.java
@@ -6,17 +6,15 @@ public class Main17626 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        int count = 1;
-        int[] ch = new int[50001];
-        for (int i = 1; i <= 50000; i++) {
-            int chNum = i - (int) Math.sqrt(i);
-            while (true) {
-                if (chNum == 0) {
-                    ch[i] = count;
-                } else {
-
-                }
+        int[] ch = new int[num + 1];
+        ch[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, ch[i - j * j]);
             }
+            ch[i] = min + 1;
         }
+        System.out.println(ch[num]);
     }
 }
