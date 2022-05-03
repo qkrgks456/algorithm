9ae36d69763bc9aaa7ce7abfefe25d30923b2256
박한솔 <6987410@naver.com>
2022-05-03 17:45:32 +0900

    일단 오늘은 여기까지

---
+++ src/baekjoon/etc5/Main6064.java
@@ -0,0 +1,14 @@
+package baekjoon.etc5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main6064 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
