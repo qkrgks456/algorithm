a848fdb645d0f5a907cfefc4902c16ebeda21975
박한솔 <6987410@naver.com>
2022-05-24 10:32:13 +0900

    한문제 컷 수학이 제일 어렵네

--- src/baekjoon/etc6/Main2477.java
+++ src/baekjoon/etc6/Main2477.java
@@ -8,7 +8,7 @@ public class Main2477 {
         int n = scanner.nextInt();
         int widthIndex = 0;
         int heightIndex = 0;
-        int max1 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
         int max2 = Integer.MIN_VALUE;
         int[] check = new int[6];
         for (int i = 0; i < 6; i++) {
@@ -17,47 +17,25 @@ public class Main2477 {
             if (direction == 1 || direction == 2) {
                 if (length > max1) {
                     widthIndex = i;
+                    max1 = length;
                 }
             } else if (direction == 3 || direction == 4) {
                 if (length > max2) {
                     heightIndex = i;
+                    max2 = length;
                 }
             }
-            check[i] = direction;
+            check[i] = length;
         }
-        // 전체 넓이는 구했다 치고, 그러면 남은 부분 직사각형은 어떻게 자르지 ?
+        // 앞뒤 인덱스를 구해야 하는데
         // 여기가 문젠대
         int area = check[widthIndex] * check[heightIndex];
-        int w1 = widthIndex + 1;
-        int w2 = widthIndex - 1;
-        int h1 = heightIndex + 1;
-        int h2 = heightIndex - 1;
-        int n1 = 0;
-        int n2 = 0;
-        if (w1 >= check.length) {
-            n1 = 0;
-        } else {
-            n1 = w1;
-        }
-        if (w2 < 0) {
-            n2 = check.length - 1;
-        } else {
-            n2 = w2;
-        }
-        int widthResult = Math.min(n1, n2);
-        n1 = 0;
-        n2 = 0;
-        if (h1 >= check.length) {
-            n1 = 0;
-        } else {
-            n1 = h1;
-        }
-        if (h2 < 0) {
-            n2 = check.length - 1;
-        } else {
-            n2 = h2;
-        }
-        int heightResult = Math.min(n1, n2);
-        System.out.println(n * (area - (widthResult * heightResult)));
+        int n1 = widthIndex + 1 >= check.length ? 0 : widthIndex + 1;
+        int n2 = widthIndex - 1 < 0 ? check.length - 1 : widthIndex - 1;
+        int n3 = heightIndex + 1 >= check.length ? 0 : heightIndex + 1;
+        int n4 = heightIndex - 1 < 0 ? check.length - 1 : heightIndex - 1;
+        int resultWidth = check[heightIndex] - Math.min(check[n1], check[n2]);
+        int resultHeight = check[widthIndex] - Math.min(check[n3], check[n4]);
+        System.out.println(n * (area - resultWidth * resultHeight));
     }
 }
---
+++ src/studymylove2/Main09.java
@@ -0,0 +1,113 @@
+package studymylove2;
+
+public class Main09 {
+    public static void main(String[] args) {
+        String[] name = {"아이언맨", "헐크  ", "캡틴  ", "토르  ", "호크아이"};
+        String hap[] = new String[5];
+        String jang[] = {"", "", "", "", ""};
+        String[] star = {"", "", "", "", ""};
+        int[] kor = {100, 20, 90, 70, 35};
+        int[] eng = {100, 50, 95, 80, 100};
+        int[] math = {100, 30, 90, 60, 100};
+        float[] avg = new float[5];
+        int[] rank = {1, 1, 1, 1, 1};
+        // 평균
+        for (int i = 0; i < name.length; i++) {
+            avg[i] = (float) (kor[i] + eng[i] + math[i]) / 3;
+        }
+        // 등수
+        for (int i = 0; i < name.length; i++) {
+            int check = 1;
+            for (int j = 0; j < name.length; j++) {
+                if (avg[i] < avg[j]) {
+                    check++;
+                }
+            }
+            rank[i] = check;
+        }
+        // 장학생
+        for (int i = 0; i < avg.length; i++) {
+            if (avg[i] >= 95) {
+                jang[i] = "장학생";
+            }
+        }
+        // hap
+        for (int i = 0; i < avg.length; i++) {
+            if (avg[i] >= 60) {
+                hap[i] = "합격";
+                boolean check = false;
+                if (kor[i] < 40) {
+                    check = true;
+                }
+                if (eng[i] < 40) {
+                    check = true;
+                }
+                if (math[i] < 40) {
+                    check = true;
+                }
+                if (check) {
+                    hap[i] = "재시험";
+                }
+            } else {
+                hap[i] = "불합격";
+            }
+        }
+        // 별
+        for (int i = 0; i < avg.length; i++) {
+            int n = (int) avg[i] / 10;
+            String s = "";
+            for (int j = 0; j < n; j++) {
+                s += "별";
+            }
+            star[i] = s;
+        }
+        int count = 0;
+        for (int i = 0; i < avg.length; i++) {
+            if (avg[i] >= 80) {
+                count++;
+            }
+        }
+
+        // 출력
+        System.out.println("이름      국어  영어  수학  평균  등수  합격여부        장학생     랭킹");
+        float max = Integer.MIN_VALUE;
+        float min = Integer.MAX_VALUE;
+        for (int i = 0; i < name.length; i++) {
+            // 최고 최저
+            max = Math.max(avg[i], max);
+            min = Math.min(avg[i], min);
+            // 정수 판별
+            if (avg[i] - (int) avg[i] == 0.0) {
+                System.out.println(name[i] + "      " + kor[i] + "  " + eng[i] + "  " + math[i] + "  "
+                        + (int) avg[i] + "  " + rank[i] + "   " + hap[i] + "      " + jang[i] + "      " + star[i]);
+            } else {
+                System.out.println(name[i] + "      " + kor[i] + "  " + eng[i] + "  " + math[i] + "  "
+                        + Math.round(avg[i] * 100) / 100.0 + "  " + rank[i] + "   " + hap[i] + "      " + jang[i] + "      " + star[i]);
+            }
+        }
+    }
+
+    private static void process_show(String[] name, int[] kor, int[] eng, int[] math, float[] avg, int[] rank, String[] pass, String[] jang, String[] star) {
+
+    }
+
+    private static void process_star(float[] avg, String[] star) {
+
+    }
+
+    private static void process_pass(float[] avg, int[] kor, int[] eng, int[] math, String[] pass) {
+
+    }
+
+    private static void process_scholar(float[] avg, String[] jang) {
+
+    }
+
+    private static void process_rank(int size, float[] avg, int[] rank) {
+
+    }
+
+    private static float prcess_aver(int kor, int eng, int math) {
+        return (float) (kor + eng + math) / 3;
+    }
+}
