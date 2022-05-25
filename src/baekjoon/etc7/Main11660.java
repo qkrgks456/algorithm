21bc4078a613f1caa12c6afbe1af49576e91b71f
박한솔 <6987410@pineone.com>
2022-05-25 16:37:51 +0900

    누적합 + DP 는 예술입니다

---
+++ src/baekjoon/etc7/Main11660.java
@@ -0,0 +1,40 @@
+package baekjoon.etc7;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main11660 {
+    // 아 DP 진짜 너무 힘들다 이게 어떻게 실버1인가
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        int[][] ints = new int[n + 1][n + 1];
+        int[][] result = new int[n + 1][n + 1];
+        for (int i = 1; i < ints.length; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 1; j < ints[i].length; j++) {
+                ints[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+        }
+
+        for (int i = 1; i < result.length; i++) {
+            for (int j = 1; j < result[i].length; j++) {
+                result[i][j] = result[i][j - 1] + result[i - 1][j] - result[i - 1][j - 1] + ints[i][j];
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int startX = Integer.parseInt(stringTokenizer.nextToken());
+            int startY = Integer.parseInt(stringTokenizer.nextToken());
+            int endX = Integer.parseInt(stringTokenizer.nextToken());
+            int endY = Integer.parseInt(stringTokenizer.nextToken());
+            stringBuilder.append(result[endX][endY] - result[endY][startX - 1] - result[startY - 1][endX] + result[startX - 1][startY - 1] + "\n");
+        }
+        System.out.println(stringBuilder);
+    }
+}
---
+++ src/studymylove3/Main05.java
@@ -0,0 +1,68 @@
+package studymylove3;
+
+import java.util.Scanner;
+
+public class Main05 {
+    public static void main(String[] args) {
+        Coffee003 a1 = new Coffee003("아메리카노", 3);
+        Coffee003 a2 = new Coffee003();
+
+        a1.show();
+        a2.input();
+        a2.show();
+    }
+}
+
+class Coffee003 {
+    String name;
+    int price;
+    int num;
+
+    @Override
+    public String toString() {
+        return "커피명: " + name + "갯수: " + num;
+    }
+
+    public Coffee003() {
+    }
+
+    public Coffee003(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+
+    public Coffee003(String name, int price, int num) {
+        this.name = name;
+        this.price = price;
+        this.num = num;
+    }
+
+    void input() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("커피명 > ");
+        name = scanner.next();
+        System.out.println("갯수 >");
+        num = scanner.nextInt();
+    }
+
+    void show() {
+        coffe_calc();
+        System.out.println("커피명 :" + name);
+        System.out.println("커피잔수 : " + num);
+        System.out.println("가격 :" + price);
+    }
+
+    void coffe_calc() {
+        switch (this.name) {
+            case "아메리카노":
+                this.price = this.num * 1000;
+                break;
+            case "카페라떼":
+                this.price = this.num * 1500;
+                break;
+            case "카푸치노":
+                this.price = this.num * 2000;
+                break;
+        }
+    }
+}
