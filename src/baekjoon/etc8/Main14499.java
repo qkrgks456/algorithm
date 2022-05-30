395d7a82cc059a8a94ffd4a7f77c9959026318c4
박한솔 <6987410@pineone.com>
2022-05-30 17:04:23 +0900

    역시 구현 킬러 한방컷

--- src/baekjoon/etc8/Main14499.java
+++ src/baekjoon/etc8/Main14499.java
@@ -7,12 +7,13 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main14499 {
-    // 1 동 2 서 3 북 4 남
-    static int[] dx = {0, 0, 0, -1, 0};
-    static int[] dy = {0, 1, -1, 0, 1};
+
+    static int[] dx = {0, 0, 0, -1, 1};
+    static int[] dy = {0, 1, -1, 0, 0};
     static int n, m;
     static int[] direction;
     static int[][] ints;
+    static StringBuilder stringBuilder = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +24,6 @@ public class Main14499 {
         int y = Integer.parseInt(stringTokenizer.nextToken());
         int k = Integer.parseInt(stringTokenizer.nextToken());
         ints = new int[n][m];
-        int[][] dice = new int[4][3];
         for (int i = 0; i < n; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             for (int j = 0; j < m; j++) {
@@ -36,36 +36,118 @@ public class Main14499 {
             direction[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
         find(x, y);
+        System.out.println(stringBuilder);
     }
 
     private static void find(int x, int y) {
         Queue<My> queue = new LinkedList<>();
-        queue.add(new My(x, y, 1, 1));
+        queue.add(new My(x, y, new Dice(0, 0, 0, 0, 0, 0)));
         int nx, ny;
+        int count = 0;
         for (int num : direction) {
+            count++;
             My my = queue.poll();
             nx = my.x + dx[num];
             ny = my.y + dy[num];
             if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
                 // 하 문제는 주사위인데 저걸 어쩐다 이거 배열 위치 교체 해줘야겠네
-
+                // 1 동 2 서 3 북 4 남
+                int right;
+                int left;
+                int bottom;
+                int top;
+                int high;
+                int row;
+                switch (num) {
+                    case 1:
+                        top = my.dice.top;
+                        bottom = my.dice.bottom;
+                        high = my.dice.left;
+                        row = my.dice.right;
+                        right = my.dice.high;
+                        left = my.dice.row;
+                        row = copy(nx, ny, row);
+                        stringBuilder.append(high + "\n");
+                        queue.add(new My(nx, ny, new Dice(right, left, bottom, top, high, row)));
+                        break;
+                    case 2:
+                        top = my.dice.top;
+                        bottom = my.dice.bottom;
+                        high = my.dice.right;
+                        row = my.dice.left;
+                        right = my.dice.row;
+                        left = my.dice.high;
+                        row = copy(nx, ny, row);
+                        stringBuilder.append(high + "\n");
+                        queue.add(new My(nx, ny, new Dice(right, left, bottom, top, high, row)));
+                        break;
+                    case 3:
+                        right = my.dice.right;
+                        left = my.dice.left;
+                        row = my.dice.top;
+                        high = my.dice.bottom;
+                        bottom = my.dice.row;
+                        top = my.dice.high;
+                        row = copy(nx, ny, row);
+                        stringBuilder.append(high + "\n");
+                        queue.add(new My(nx, ny, new Dice(right, left, bottom, top, high, row)));
+                        break;
+                    case 4:
+                        right = my.dice.right;
+                        left = my.dice.left;
+                        row = my.dice.bottom;
+                        high = my.dice.top;
+                        bottom = my.dice.high;
+                        top = my.dice.row;
+                        row = copy(nx, ny, row);
+                        stringBuilder.append(high + "\n");
+                        queue.add(new My(nx, ny, new Dice(right, left, bottom, top, high, row)));
+                        break;
+                }
             } else {
                 queue.add(my);
             }
         }
+    }
+
+    public static int copy(int nx, int ny, int row) {
+        if (ints[nx][ny] == 0) {
+            ints[nx][ny] = row;
+            return row;
+        } else {
+            int num = ints[nx][ny];
+            ints[nx][ny] = 0;
+            return num;
+        }
+    }
+}
+
+class Dice {
+    int right;
+    int left;
+    int bottom;
+    int top;
+    int high;
+    int row;
+
+    public Dice(int right, int left, int bottom, int top, int high, int row) {
+        this.right = right;
+        this.left = left;
+        this.bottom = bottom;
+        this.top = top;
+        this.high = high;
+        this.row = row;
     }
 }
 
 class My {
     int x;
     int y;
-    int diceX;
-    int diceY;
+    Dice dice;
 
-    public My(int x, int y, int diceX, int diceY) {
+    public My(int x, int y, Dice dice) {
         this.x = x;
         this.y = y;
-        this.diceX = diceX;
-        this.diceY = diceY;
+        this.dice = dice;
     }
 }
