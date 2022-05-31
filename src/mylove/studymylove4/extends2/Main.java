b4c4afee60fa6d074081576f960244fbd6863029
박한솔 <6987410@pineone.com>
2022-05-31 12:57:02 +0900

    톱나바퀴 후 ..

---
+++ src/mylove/studymylove4/extends2/Main.java
@@ -0,0 +1,12 @@
+package mylove.studymylove4.extends2;
+
+public class Main {
+    public static void main(String[] args) {
+        Fruit.line();
+        Fruit fruit = new Fruit();
+        Fruit.line();
+        Apple apple = new Apple();
+        Fruit.line();
+        AppleJam appleJam = new AppleJam();
+    }
+}
---
+++ src/mylove/studymylove4/extends3/Main.java
@@ -0,0 +1,15 @@
+package mylove.studymylove4.extends3;
+
+public class Main {
+    public static void main(String[] args) {
+        Fruit2.line();
+        Fruit2 fruit = new Fruit2();
+        fruit.star(0);
+        Fruit2.line();
+        Apple2 apple = new Apple2();
+        apple.star(4);
+        Fruit2.line();
+        AppleJam2 appleJam = new AppleJam2();
+        appleJam.star(5);
+    }
+}
