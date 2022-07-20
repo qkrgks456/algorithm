49aa412bee610b1075b5c9c154f790333f2dad33
박한솔 <6987410@pineone.com>
2022-07-20 15:14:31 +0900

    소수점 조건좀 티나게 주지 진짜 햇갈리게 내냐 ㅋㅋ 그래도 한방에 컷

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,26 +1,11 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] arr = new int[9];
-        int max = Integer.MIN_VALUE;
-        int num = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = Math.max(arr[i], max);
-                num = i + 1;
-            }
-        }
-        System.out.println(max);
-        System.out.println(num);
+        List<Integer> list = new ArrayList<>();
+        System.out.println(list.size());
     }
 }
