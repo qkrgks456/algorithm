00864242c87c72b4a959315298f16c6a7c2a103a
박한솔 <6987410@naver.com>
2022-07-27 17:45:07 +0900

    아 map 실수는 좀 그렇잖아 일단 클리어

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,7 +1,52 @@
 package baekjoon;
 
+import java.awt.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Map<Integer, Map<Integer, Point>> map = new HashMap<>();
+        map.get(0).get(1);
 
+    }
+}
+
+class MyComparator implements Comparator<Person> {
+    int result = 0;
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        if (o1.x > o2.x) {
+            result = 1;
+        } else if (o1.x < o2.x) {
+            result = -1;
+        } else {
+            if (o1.y > o2.y) {
+                result = 1;
+            } else if (o1.y < o2.y) {
+                result = -1;
+            }
+        }
+        return result;
+    }
+}
+
+class Person {
+    int x;
+    int y;
+
+    public Person(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
--- src/baekjoon/etc9/Main19238.java
+++ src/baekjoon/etc9/Main19238.java
@@ -3,16 +3,16 @@ package baekjoon.etc9;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
 import java.util.List;
+import java.util.Queue;
+import java.util.*;
 
 public class Main19238 {
     static int N, M, G;
     static int[][] ints, ch, distance;
-    static Car car;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static Map<Integer, Map<Integer, Point>> map = new HashMap<>();
+    static Map<Key, Point> map = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -30,7 +30,6 @@ public class Main19238 {
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int x = Integer.parseInt(stringTokenizer.nextToken()) - 1;
         int y = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-        car = new Car(x, y, G);
         for (int i = 0; i < M; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int sx = Integer.parseInt(stringTokenizer.nextToken()) - 1;
@@ -38,14 +37,23 @@ public class Main19238 {
             int ex = Integer.parseInt(stringTokenizer.nextToken()) - 1;
             int ey = Integer.parseInt(stringTokenizer.nextToken()) - 1;
             ints[sx][sy] = 2;
-            Map<Integer, Point> maps = new HashMap<>();
-            maps.put(sy, new Point(ex, ey));
-            map.put(sx, maps);
+            map.put(new Key(sx, sy), new Point(ex, ey));
         }
-        BFS(x, y);
+
+        for (int i = 0; i < M; i++) {
+            Point point = startBFS(x, y);
+            x = point.x;
+            y = point.y;
+        }
+        System.out.println(G);
     }
 
-    public static void BFS(int x, int y) {
+    public static Point startBFS(int x, int y) {
+        if (ints[x][y] == 2) {
+            ints[x][y] = 0;
+            Point end = map.get(new Key(x, y));
+            return endBFS(x, y, end);
+        }
         Queue<Point> queue = new LinkedList<>();
         ch = new int[N][N];
         distance = new int[N][N];
@@ -63,31 +71,67 @@ public class Main19238 {
                     if (nx >= 0 && ny >= 0 && nx < N && ny < N && ch[nx][ny] == 0 && ints[nx][ny] != 1) {
                         ch[nx][ny] = 1;
                         distance[nx][ny] = distance[point.x][point.y] + 1;
+                        queue.add(new Point(nx, ny));
+                        // 같은거리면 리스트에 넣기
                         if (ints[nx][ny] == 2) {
                             list.add(new Person(nx, ny));
                         }
-                        queue.add(new Point(nx, ny));
                     }
                 }
             }
+            // 출발 손님이 있다면
             if (list.size() > 0) {
-                Collections.sort(list, new MyComparator());
-                System.out.println(list);
-                return;
+                // 정렬해서 0번이 행이 작고 같다면 열이 가장 작은 손님
+                list.sort(new MyComparator());
+                Person start = list.get(0);
+                // 연료 소모시켜서 확인하기
+                G -= distance[start.x][start.y];
+                ints[start.x][start.y] = 0;
+                if (G < 0) {
+                    System.out.println(-1);
+                    System.exit(0);
+                }
+                Point end = map.get(new Key(start.x, start.y));
+                return endBFS(start.x, start.y, end);
             }
         }
+        // 없으면 -1 종료
+        System.out.println(-1);
+        System.exit(0);
+        return null;
     }
-}
 
-class Car {
-    int x;
-    int y;
-    int gas;
-
-    public Car(int x, int y, int gas) {
-        this.x = x;
-        this.y = y;
-        this.gas = gas;
+    private static Point endBFS(int x, int y, Point end) {
+        Queue<Point> queue = new LinkedList<>();
+        ch = new int[N][N];
+        distance = new int[N][N];
+        ch[x][y] = 1;
+        queue.add(new Point(x, y));
+        int nx, ny;
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                nx = point.x + dx[i];
+                ny = point.y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N && ch[nx][ny] == 0 && ints[nx][ny] != 1) {
+                    ch[nx][ny] = 1;
+                    distance[nx][ny] = distance[point.x][point.y] + 1;
+                    queue.add(new Point(nx, ny));
+                    if (nx == end.x && ny == end.y) {
+                        G -= distance[nx][ny];
+                        if (G < 0) {
+                            System.out.println(-1);
+                            System.exit(0);
+                        }
+                        G += distance[nx][ny] * 2;
+                        return end;
+                    }
+                }
+            }
+        }
+        System.out.println(-1);
+        System.exit(0);
+        return null;
     }
 }
 
@@ -96,14 +140,14 @@ class MyComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.x < o2.x) {
+        if (o1.x > o2.x) {
             result = 1;
-        } else if (o1.x > o2.x) {
+        } else if (o1.x < o2.x) {
             result = -1;
         } else {
-            if (o1.y < o2.y) {
+            if (o1.y > o2.y) {
                 result = 1;
-            } else if (o1.y > o2.y) {
+            } else if (o1.y < o2.y) {
                 result = -1;
             }
         }
@@ -127,4 +171,31 @@ class Person {
                 ", y=" + y +
                 '}';
     }
+}
+
+class Key {
+
+    private final int x;
+    private final int y;
+
+    public Key(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Key)) return false;
+        Key key = (Key) o;
+        return x == key.x && y == key.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
 }
