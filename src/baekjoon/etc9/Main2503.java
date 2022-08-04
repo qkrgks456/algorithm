75be9559db4c24584d94cc0219ec1cc04e8b6dee
박한솔 <6987410@naver.com>
2022-08-04 17:44:44 +0900

    볼체크 해야됨

--- src/baekjoon/etc9/Main2503.java
+++ src/baekjoon/etc9/Main2503.java
@@ -2,36 +2,73 @@ package baekjoon.etc9;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main2503 {
+    static int result = 0;
+    static List<Quiz> list = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
         // 아 그러네 111 ~ 999 까지 포문 돌면서 조건 맞는지 확인하면 되네 ㅅㅂ
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
-        loop:
-        for (int i = 111; i <= 999; i++) {
-            for (int j = 0; j < n; j++) {
-                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                String[] strings = stringTokenizer.nextToken().split("");
-                int strike = Integer.parseInt(stringTokenizer.nextToken());
-                int ball = Integer.parseInt(stringTokenizer.nextToken());
-                // 스트라이크 체크
-                if (strike != 0) {
-                    int count = 0;
-                    for (String s : strings) {
-                        String checkStr = String.valueOf(i);
-                        if (checkStr.contains(s)) count++;
-                    }
-                    if (count != strike) {
-                        continue loop;
-                    }
-                }
-                // 볼체크
-                if (ball != 0) {
+        for (int j = 0; j < n; j++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int num = Integer.parseInt(stringTokenizer.nextToken());
+            int strike = Integer.parseInt(stringTokenizer.nextToken());
+            int ball = Integer.parseInt(stringTokenizer.nextToken());
+            list.add(new Quiz(num, strike, ball));
+        }
+        String[] s1 = new String[3];
+        DFS(0, s1);
+        System.out.println(result);
+    }
 
+    public static void DFS(int level, String[] s1) {
+        if (level == 3) {
+            checkNum(s1);
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            s1[level] = String.valueOf(i);
+            DFS(level + 1, s1);
+        }
+    }
+
+    public static void checkNum(String[] s1) {
+        String str = s1.toString();
+        for (Quiz quiz : list) {
+            String[] s = String.valueOf(quiz.num).split("");
+            int count = 0;
+            int count2 = 0;
+            //TODO 볼체크 해야됨 ㅋㅋ
+            for (int j = 0; j < 3; j++) {
+                if (s[j].equals(s1[j])) {
+                    count++;
                 }
             }
+            if (quiz.strike != count) {
+                return;
+            }
+            if (quiz.ball != count2) {
+                return;
+            }
         }
+        result++;
+    }
+}
+
+class Quiz {
+    int num;
+    int strike;
+    int ball;
+
+    public Quiz(int num, int strike, int ball) {
+        this.num = num;
+        this.strike = strike;
+        this.ball = ball;
     }
 }
