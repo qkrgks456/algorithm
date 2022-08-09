af0e2c53379f4a8eca5cf966c643b90e09ba4c9c
박한솔 <6987410@pineone.com>
2022-08-09 15:10:17 +0900

    진짜 상어 찢어 죽이고싶었다

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,20 +1,8 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        List<Tree> list = new ArrayList<>();
-        list.add(new Tree(1, 2, 1));
-        list.add(new Tree(1, 1, 3));
-        list.add(new Tree(6, 5, 2));
-        list.add(new Tree(5, 2, 3));
-        list.add(new Tree(5, 2, 2));
-        list.add(new Tree(5, 2, 1));
-        Collections.sort(list);
-        System.out.println(list);
+        System.out.println(1 / 4);
     }
 }
 
