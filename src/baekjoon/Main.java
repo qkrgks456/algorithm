555dc1e777459a9fd8dcec58d30a968f26afffd6
박한솔 <6987410@pineone.com>
2022-08-12 11:20:12 +0900

    와 개 빡시네정말

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,8 +1,16 @@
 package baekjoon;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(1 / 4);
+        Deque<Integer> deque = new LinkedList<>();
+        deque.add(1);
+        deque.add(2);
+        deque.add(3);
+        deque.addFirst(deque.pollLast());
+        System.out.println(deque);
     }
 }
 
