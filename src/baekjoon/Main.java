bb5e0762160d269a267f027964a1f7962c90b69e
박한솔 <6987410@pineone.com>
2022-08-19 15:58:05 +0900

    와 한방 기분 너무 좋다 진짜 어려웠다

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,19 +1,58 @@
 package baekjoon;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Deque<Integer> deque = new LinkedList<>();
-        deque.add(1);
-        deque.add(2);
-        deque.add(3);
-        deque.addFirst(deque.pollLast());
-        System.out.println(deque);
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        for (int i = 1; i < list.size(); i++) {
+            list.remove(i);
+            i--;
+        }
+        System.out.println(list);
+
     }
 }
 
+
+class Test {
+    int no;
+    int num;
+
+    public Test(int no, int num) {
+        this.no = no;
+        this.num = num;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "no=" + no +
+                ", num=" + num +
+                '}';
+    }
+}
 
 class Tree implements Comparable<Tree> {
     int x;
--- src/baekjoon/etc10/Main19237.java
+++ src/baekjoon/etc10/Main19237.java
@@ -2,66 +2,155 @@ package baekjoon.etc10;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main19237 {
     static int dx[] = {0, -1, 1, 0, 0};
     static int dy[] = {0, 0, 0, -1, 1};
+    static int N, M, k;
+    static Smell[][] smells;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
-        int k = Integer.parseInt(stringTokenizer.nextToken());
-        List<Shark> list = new ArrayList<>();
-        int[][] ints = new int[N][N];
-        Small[][] smalls = new Small[N][N];
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        M = Integer.parseInt(stringTokenizer.nextToken());
+        k = Integer.parseInt(stringTokenizer.nextToken());
+        Shark[] list = new Shark[M + 1];
+        List<Integer>[][] ints = new ArrayList[N][N];
+        smells = new Smell[N][N];
         for (int i = 0; i < N; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             for (int j = 0; j < N; j++) {
                 int num = Integer.parseInt(stringTokenizer.nextToken());
+                ints[i][j] = new ArrayList<>();
                 if (num != 0) {
-                    list.add(new Shark(i, j, 0, num));
-                    smalls[i][j] = new Small(num, k);
-                    ints[i][j] = num;
+                    list[num] = new Shark(i, j, 0, num);
+                    smells[i][j] = new Smell(num, k);
+                    ints[i][j].add(num);
                 }
             }
         }
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= M; i++) {
             int direction = Integer.parseInt(stringTokenizer.nextToken());
-            Shark shark = list.get(i);
+            Shark shark = list[i];
             shark.d = direction;
         }
-
-        for (int i = 0; i < M; i++) {
-            Shark shark = list.get(i);
-            int[][] priority = new int[5][5];
+        int[][][] priority = new int[M + 1][5][5];
+        for (int i = 1; i <= M; i++) {
             for (int j = 1; j <= 4; j++) {
                 stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                for (int l = 0; l < 4; l++) {
-                    priority[j][l] = Integer.parseInt(stringTokenizer.nextToken());
+                for (int l = 1; l <= 4; l++) {
+                    priority[i][j][l] = Integer.parseInt(stringTokenizer.nextToken());
                 }
             }
-            shark.priority = priority;
         }
+        for (int i = 1; i <= M; i++) {
+            Shark shark = list[i];
+            shark.priority = priority[i];
+        }
+
         for (int i = 1; i <= 1000; i++) {
-            // 일단 시작하면 냄새 기간 깎기
-            for (int j = 0; j < N; j++) {
-                for (int l = 0; l < N; l++) {
-                    if (smalls[j][l] != null) {
-                        smalls[j][l].count--;
-                        if (smalls[j][l].count == 0) {
-                            smalls[j][l] = null;
+            // 상어 이동
+            for (Shark shark : list) {
+                if (shark == null) {
+                    continue;
+                }
+                int nx, ny;
+                boolean smellCheck = false;
+                // 4방향 내 냄새 몇개 있는지 확인
+                for (int j = 1; j <= 4; j++) {
+                    nx = shark.x + dx[j];
+                    ny = shark.y + dy[j];
+                    if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                        Smell smell = smells[nx][ny];
+                        if (smell == null) {
+                            smellCheck = true;
+                        }
+                    }
+                }
+                int[] direction = shark.getDirection();
+                if (smellCheck) {
+                    // 주위에 아무냄새가 없는 칸이 존재함
+                    for (int j = 1; j < direction.length; j++) {
+                        nx = shark.x + dx[direction[j]];
+                        ny = shark.y + dy[direction[j]];
+                        if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                            if (smells[nx][ny] == null) {
+                                // 실제 이동
+                                Integer integer = shark.num;
+                                ints[shark.x][shark.y].remove(integer);
+                                ints[nx][ny].add(shark.num);
+                                shark.x = nx;
+                                shark.y = ny;
+                                shark.d = direction[j];
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    // 주위가 모두 냄새가 있는 칸
+                    for (int j = 1; j < direction.length; j++) {
+                        nx = shark.x + dx[direction[j]];
+                        ny = shark.y + dy[direction[j]];
+                        if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                            if (shark.num == smells[nx][ny].num) {
+                                // 실제 이동
+                                Integer integer = shark.num;
+                                ints[shark.x][shark.y].remove(integer);
+                                ints[nx][ny].add(shark.num);
+                                shark.x = nx;
+                                shark.y = ny;
+                                shark.d = direction[j];
+                                break;
+                            }
                         }
                     }
                 }
             }
-        }
+            // 이동후에 스멜 제거
+            for (int j = 0; j < N; j++) {
+                for (int l = 0; l < N; l++) {
+                    if (smells[j][l] != null) {
+                        smells[j][l].count--;
+                        if (smells[j][l].count == 0) {
+                            smells[j][l] = null;
+                        }
+                    }
+                }
+            }
 
+            // 이동 끝났으면 중복되는애들 제일 낮은애 빼고 없애기
+            // 플러스 스멜 추가해주기
+            for (int j = 0; j < N; j++) {
+                for (int l = 0; l < N; l++) {
+                    if (ints[j][l].size() > 0) {
+                        Collections.sort(ints[j][l]);
+                        for (int m = 1; m < ints[j][l].size(); m++) {
+                            List<Integer> integers = ints[j][l];
+                            list[integers.get(m)] = null;
+                            integers.remove(m);
+                            m--;
+                        }
+                        smells[j][l] = new Smell(ints[j][l].get(0), k);
+                    }
+                }
+            }
+            int count = 0;
+            for (int j = 0; j < N; j++) {
+                for (int l = 0; l < N; l++) {
+                    if (ints[j][l].size() > 0) {
+                        count++;
+                    }
+                }
+            }
+            if (count == 1) {
+                System.out.println(i);
+                System.exit(0);
+            }
+        }
+        System.out.println(-1);
     }
 }
 
@@ -88,12 +177,20 @@ class Shark {
     }
 }
 
-class Small {
+class Smell {
     int num;
     int count;
 
-    public Small(int num, int count) {
+    public Smell(int num, int count) {
         this.num = num;
         this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Smell{" +
+                "num=" + num +
+                ", count=" + count +
+                '}';
     }
 }
