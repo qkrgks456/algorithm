b457de7cd642c2cab6993eb321c8016abb02faf2
박한솔 <6987410@pineone.com>
2022-09-14 16:31:35 +0900

    몸풀기 ~

---
+++ src/baekjoon/etc10/Main24060.java
@@ -0,0 +1,21 @@
+package baekjoon.etc10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main24060 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int k = Integer.parseInt(stringTokenizer.nextToken());
+        int[] ints = new int[n];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+            ints[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        int q =n / 2;
+
+    }
+}
