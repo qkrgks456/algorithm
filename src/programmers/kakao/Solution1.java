dc1dc3595a8e3db94a9bbac1787b98bc78d929d1
박한솔 <6987410@naver.com>
2022-09-15 16:02:09 +0900

    카카오 수련 시작

--- src/programmers/kakao/Solution1.java
+++ src/programmers/kakao/Solution1.java
@@ -1,8 +1,40 @@
 package programmers.kakao;
 
+import java.util.*;
+
 public class Solution1 {
+
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = {};
+        Map<String, Set<String>> map = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        for (String s : id_list) {
+            map.put(s, new HashSet<>());
+            map2.put(s, 0);
+        }
+
+        for (String s : report) {
+            StringTokenizer stringTokenizer = new StringTokenizer(s);
+            String id = stringTokenizer.nextToken();
+            String reportId = stringTokenizer.nextToken();
+            Set<String> set = map.get(id);
+            map.put(id, set);
+            if (!set.contains(reportId)) {
+                int n = map2.get(reportId);
+                map2.put(reportId, ++n);
+            }
+            set.add(reportId);
+        }
+        answer = new int[id_list.length];
+        for (String s : map2.keySet()) {
+            if (map2.get(s) >= k) {
+                for (int i = 0; i < id_list.length; i++) {
+                    if (map.get(id_list[i]).contains(s)) {
+                        answer[i]++;
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
