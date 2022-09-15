21895ecb2214234698fcc7aaa2dcd655d5b5724b
박한솔 <6987410@naver.com>
2022-09-15 15:04:10 +0900

    뭐 이런 실수를 하니

---
+++ src/programmers/kakao/Solution1.java
@@ -0,0 +1,8 @@
+package programmers.kakao;
+
+public class Solution1 {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+}
