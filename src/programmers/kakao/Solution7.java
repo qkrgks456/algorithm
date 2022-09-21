0a1e4289052dc7c574990c46f1ba708751380c33
박한솔 <6987410@naver.com>
2022-09-21 18:05:49 +0900

    다시 손댐

--- src/programmers/kakao/Solution7.java
+++ src/programmers/kakao/Solution7.java
@@ -6,23 +6,46 @@ public class Solution7 {
 
     public int solution(int[][] board, int[] aloc, int[] bloc) {
         int answer = -1;
-        DFS(aloc[0], aloc[1], bloc[0], bloc[1], "a", board);
+        DFS(aloc[0], aloc[1], bloc[0], bloc[1], "a", board, 0);
         return answer;
     }
 
-    public static void DFS(int ax, int ay, int bx, int by, String check, int[][] board) {
-        if (!moveCheck(ax, ay, board)) {
+    public static void DFS(int ax, int ay, int bx, int by, String check, int[][] board, int score) {
+        // 둘중 하나라도 움직일 수 없다면
+        if (!moveCheck(ax, ay, board) || !moveCheck(bx, by, board)) {
+
             return;
         }
-        if (!moveCheck(bx, by, board)) {
+        // 둘의 자리가 같아서 다음번에 이동하면 끝남
+        if (ax == bx && ay == by) {
             return;
+        }
+
+
+        int nx, ny;
+        int x, y;
+        if (check.equals("a")) {
+            x = ax;
+            y = ay;
+        } else {
+            x = bx;
+            y = by;
+        }
+        for (int i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+            board[x][y] = 0;
+            score++;
+            DFS(nx, ny, bx, by, "b", board, score);
+            board[x][y] = 1;
+            score--;
         }
 
 
     }
 
-    public static boolean moveCheck(int x, int y, int[][] check) {
-        if (x >= 0 && y >= 0 && x < check.length && y < check[0].length && check[x][y] == 1) {
+    public static boolean moveCheck(int x, int y, int[][] board) {
+        if (x >= 0 && y >= 0 && x < board.length && y < board[0].length && board[x][y] == 1) {
             return true;
         }
         return false;
