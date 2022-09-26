81547d5895e72b60f9dd7fe72b063f92de29fbcc
박한솔 <6987410@pineone.com>
2022-09-26 09:53:13 +0900

    겸손하게 초심으로 돌아가서 시작하겠습니다

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -1,26 +1,10 @@
 package programmers;
 
-import java.util.*;
-
 public class Main {
-    static int max;
-    static char[] check;
-    static char[] chars = new char[26];
-    static List<Map<String, Integer>> list;
-    static List<String> result = new ArrayList<>();
 
     public static void main(String[] args) {
-        String s = "asd and tre and hfg 100";
-        s = s.replaceAll(" and ","");
-        String[] strings = s.split(" ");
-        for (String string : strings) {
-            System.out.println(string);
-        }
-    }
 
+
+    }
 }
 
-// 1 2 3 4 5
-// 2 4 6 8 10
-// 3
-// 4
