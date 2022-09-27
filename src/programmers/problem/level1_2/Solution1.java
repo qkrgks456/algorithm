0697363bdf5db4711e15334e66d2d18920ac5d57
박한솔 <6987410@naver.com>
2022-09-27 17:07:58 +0900

    level1 빠른 순삭테크

---
+++ src/programmers/problem/level1_2/Solution1.java
@@ -0,0 +1,15 @@
+package programmers.problem.level1_2;
+
+public class Solution1 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int result = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                result += absolutes[i];
+            } else {
+                result -= absolutes[i];
+            }
+        }
+        return result;
+    }
+}
---
+++ src/programmers/problem/level1_2/Solution7.java
@@ -0,0 +1,4 @@
+package programmers.problem.level1_2;
+
+public class Solution7 {
+}
