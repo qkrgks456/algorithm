cf12f7a30f05712c0c9a106ec7b2ea80f992675e
박한솔 <6987410@pineone.com>
2022-09-30 17:50:56 +0900

    오늘은 여기까지

--- src/programmers/problem/level1_5/Solution5.java
+++ src/programmers/problem/level1_5/Solution5.java
@@ -2,7 +2,6 @@ package programmers.problem.level1_5;
 
 public class Solution5 {
     public boolean solution(String s) {
-        boolean answer = true;
-        return answer;
+        return s.matches("^\\d{4}$|^\\d{6}$");
     }
 }
