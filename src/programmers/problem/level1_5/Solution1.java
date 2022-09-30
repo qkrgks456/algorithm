7fc2863bd3f8c8c23e6616dabeb2a846c1df5244
박한솔 <6987410@pineone.com>
2022-09-30 17:12:48 +0900

    중간 마무리

---
+++ src/programmers/problem/level1_5/Solution1.java
@@ -0,0 +1,21 @@
+package programmers.problem.level1_5;
+
+import java.util.Stack;
+
+public class Solution1 {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            int check = stack.peek();
+            if (check != arr[i]) {
+                stack.add(arr[i]);
+            }
+        }
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
---
+++ src/programmers/problem/level1_5/Solution4.java
@@ -0,0 +1,12 @@
+package programmers.problem.level1_5;
+
+public class Solution4 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                 arr1[i][j] += arr2[i][j];
+            }
+        }
+        return arr1;
+    }
+}
