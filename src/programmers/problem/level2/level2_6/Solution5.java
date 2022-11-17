866e79f888e6a9f398b0b2713307c85afd6cc4ed
박한솔 <6987410@naver.com>
2022-11-17 17:18:36 +0900

    라고하며 2문제 더풀어보리기 ~

--- src/programmers/problem/level2/level2_6/Solution5.java
+++ src/programmers/problem/level2/level2_6/Solution5.java
@@ -1,9 +1,21 @@
 package programmers.problem.level2.level2_6;
 
+import java.util.Arrays;
+
 public class Solution5 {
     public String solution(int[] numbers) {
-        
-        String answer = "";
-        return answer;
+        String[] strings = new String[numbers.length];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(strings, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        if (strings[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < strings.length; i++) {
+            stringBuilder.append(strings[i]);
+        }
+        return stringBuilder.toString();
     }
 }
