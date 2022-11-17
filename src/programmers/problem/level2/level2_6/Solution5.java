c33df9d834891155a04a393702eb599e75951bca
박한솔 <6987410@naver.com>
2022-11-17 16:33:56 +0900

    오늘은 여기까지

---
+++ src/programmers/problem/level2/level2_6/Solution5.java
@@ -0,0 +1,9 @@
+package programmers.problem.level2.level2_6;
+
+public class Solution5 {
+    public String solution(int[] numbers) {
+        
+        String answer = "";
+        return answer;
+    }
+}
