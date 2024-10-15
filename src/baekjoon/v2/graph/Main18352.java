4b6cb307425ea076426d12d644e95b4d5600541f
박한솔 <6987410@pineone.com>
2024-10-16 01:31:06 +0900

    구현,누적합

---
+++ src/baekjoon/v2/graph/Main18352.java
@@ -0,0 +1,7 @@
+package baekjoon.v2.graph;
+
+public class Main18352 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/baekjoon/v2/prefixsum/Main2003.java
+++ src/baekjoon/v2/prefixsum/Main2003.java
@@ -11,5 +11,33 @@ public class Main2003 {
         StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int M = Integer.parseInt(stringTokenizer.nextToken());
+        int[] ints = new int[N];
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        stringTokenizer = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            ints[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        int result = 0;
+        while (true) {
+            if (sum < M) {
+                if (end < N) {
+                    sum += ints[end];
+                    end++;
+                } else {
+                    break;
+                }
+            } else if (sum == M) {
+                result++;
+                sum -= ints[start];
+                start++;
+            } else {
+                sum -= ints[start];
+                start++;
+            }
+
+        }
+        System.out.println(result);
     }
 }
