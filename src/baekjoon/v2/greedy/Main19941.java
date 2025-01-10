0bc48aed8e7b17bf9f636fc98d94697c106f6012
박한솔 <6987410@naver.com>
2025-01-10 10:14:00 +0900

    그리디 - 햄버거 분배

--- src/baekjoon/v2/greedy/Main19941.java
+++ src/baekjoon/v2/greedy/Main19941.java
@@ -10,6 +10,23 @@ public class Main19941 {
         StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int k = Integer.parseInt(stringTokenizer.nextToken());
-
+        String s = br.readLine();
+        int[] check = new int[n];
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int start = i - k;
+            int end = i + k;
+            if (s.charAt(i) == 'P') {
+                for (int j = start; j <= end; j++) {
+                    if (j < 0 || j >= n || check[j] == 1) continue;
+                    if (s.charAt(j) == 'H') {
+                        check[j] = 1;
+                        result++;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
